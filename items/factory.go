757aaaeae242415e2b4e0a35df1bd1028aabced2
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2019-10-19 19:16:18 +0200

    Colisions with falling objects added but works badly :D

---
+++ items/factory.go
@@ -0,0 +1,33 @@
+components {
+  id: "script"
+  component: "/items/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "paper-1-factory"
+  type: "factory"
+  data: "prototype: \"/items/paper-1.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
