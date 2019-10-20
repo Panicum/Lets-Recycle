945322102646ac5e917b119b43463fb216121c8d
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2019-10-20 19:36:59 +0200

    Collection items start working.

---
+++ player/factory.go
@@ -0,0 +1,33 @@
+components {
+  id: "paper-trashcan"
+  component: "/player/paper-trashcan.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/player/paper-trashcan.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
