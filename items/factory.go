b40841c54f9dece4f524191a6251f386441235f8
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2019-10-20 20:20:16 +0200

    Second falling item added.

--- items/factory.go
+++ items/factory.go
@@ -31,3 +31,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "paper-2-factory"
+  type: "factory"
+  data: "prototype: \"/items/paper-2.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
