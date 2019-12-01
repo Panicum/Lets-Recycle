e023e36f8f28f9b54fa770e2ffbfad97b6cbb422
matikap2 <matikap2@gmail.com>
2019-12-01 19:43:24 +0100

    changed bg

---
+++ main/background.go
@@ -0,0 +1,35 @@
+components {
+  id: "background"
+  component: "/main/background.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/bgAtlas.atlas\"\n"
+  "default_animation: \"city_background\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
